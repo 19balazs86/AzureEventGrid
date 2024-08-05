89c089d1e355a83d5a97b847ffa2ad7e0831d6f1
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-05 09:55:19 +0200

    Add MqttClientExample

---
+++ ConsoleAppMQTT/MqttClientExample.cs
@@ -0,0 +1,64 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+
+namespace ConsoleAppMQTT;
+
+public static class MqttClientExample
+{
+    public static async Task Run(CancellationToken cancelToken)
+    {
+        MqttClientOptions clientOptions = Settings.GetMqttClientOptions();
+
+        var mqttFactory = new MqttFactory();
+
+        using IMqttClient client = mqttFactory.CreateMqttClient();
+
+        MqttClientConnectResult connectResult = await client.ConnectAsync(clientOptions);
+
+        Console.WriteLine($"ConnectResult: {connectResult.ResultCode}");
+
+        // --> Start: Publisher
+        Task publisherTask = publisher(client, cancelToken);
+
+        // --> Subscribe to topic
+        await client.SubscribeAsync(Settings.Topic);
+
+        client.ApplicationMessageReceivedAsync += client_MessageReceived;
+
+        // --> Wait for the publisher until cancelToken closed
+        await publisherTask;
+
+        // --> Disconnect
+        // Send a clean disconnect to the server by calling the DisconnectAsync(...)
+        // Without disconnect, the TCP connection gets dropped and the server will handle this as a non clean disconnect
+        await client.DisconnectAsync();
+    }
+
+    private static async Task client_MessageReceived(MqttApplicationMessageReceivedEventArgs arg)
+    {
+        string topic   = arg.ApplicationMessage.Topic;
+        string payload = arg.ApplicationMessage.ConvertPayloadToString();
+
+        await Console.Out.WriteLineAsync($"'{topic}' - Payload: '{payload}'");
+
+        // await arg.AcknowledgeAsync(CancellationToken.None); // Not sure about it
+    }
+
+    private static async Task publisher(IMqttClient client, CancellationToken cancelToken)
+    {
+        while (!cancelToken.IsCancellationRequested)
+        {
+            string payload = $"Current time: {DateTime.Now.ToLongTimeString()}";
+
+            MqttApplicationMessage message = new MqttApplicationMessageBuilder()
+                .WithTopic(Settings.Topic)
+                .WithPayload(payload)
+                .Build();
+
+            MqttClientPublishResult publishResult = await client.PublishAsync(message);
+
+            await Task.WhenAny(Task.Delay(2_000, cancelToken)); // No TaskCanceledException
+        }
+    }
+}
---
+++ ConsoleAppMQTT/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleAppMQTT;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var cancellationTokenSource = new CancellationTokenSource();
+
+        Task exampleTask = MqttClientExample.Run(cancellationTokenSource.Token);
+        // Task exampleTask = MqttManagedClientExample.Run(cancellationTokenSource.Token);
+
+        await Task.WhenAny(exampleTask, readKey());
+
+        cancellationTokenSource.Cancel();
+
+        Console.WriteLine("Program is closing");
+
+        await exampleTask; // Wait for closing the connection
+
+        Console.WriteLine("End of MQTT example");
+    }
+
+    private static Task readKey()
+    {
+        Console.WriteLine("Press any key to stop");
+        Console.ReadKey(true);
+
+        return Task.CompletedTask;
+    }
+}
